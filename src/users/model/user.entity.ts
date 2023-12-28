ace98128781ee0cee7fb11191ed3fb136d04ee44
TAZIReda <tazir_reda@outlook.fr>
2023-12-28 10:28:42 +0100

    User Entity & User Interface

---
+++ src/users/model/user.entity.ts
@@ -0,0 +1,34 @@
+import { Part } from 'src/parts/model/part.interface';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { UserRole } from './user.interface';
+import { PartEntity } from 'src/parts/model/part.entity';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password?: string;
+
+  @Column()
+  address?: string;
+
+  @Column()
+  enterprise?: string;
+
+  @Column()
+  phone?: number;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role?: UserRole;
+
+  @OneToMany((type) => PartEntity, (PartEntity) => PartEntity.user)
+  parts?: Part[];
+}
---
+++ src/users/model/user.interface.ts
@@ -0,0 +1,18 @@
+import { Part } from "src/parts/model/part.interface";
+
+export interface User {
+    id?: number;
+    name?: string;
+    email?: string;
+    password?: string;
+    address?: string;
+    enterprise?: string;
+    phone?: number;
+    role?: UserRole;
+    parts?: Part[];
+}
+
+export enum UserRole{
+    ADMIN = "admin",
+    USER= "user"
+}
