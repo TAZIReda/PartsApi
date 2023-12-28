c25b46f6337367290a8058b33a5e8cd53318651e
TAZIReda <tazir_reda@outlook.fr>
2023-12-28 09:39:13 +0100

    config typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModuleOptions, TypeOrmModule } from '@nestjs/typeorm';
+import { PartsModule } from './parts/parts.module';
+import { UsersModule } from './users/users.module';
+
+const ormOptions: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'blog',
+  autoLoadEntities : true,
+  synchronize : true
+}
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormOptions),
+    PartsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
