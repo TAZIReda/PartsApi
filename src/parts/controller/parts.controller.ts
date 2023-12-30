f4a162731fc994e566ab13689ffd5dd23afff44d
TAZIReda <tazir_reda@outlook.fr>
2023-12-30 10:15:43 +0100

    create API & Servoce Parts

--- src/parts/controller/parts.controller.ts
+++ src/parts/controller/parts.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Request } from '@nestjs/common';
+import { Part } from '../model/part.interface';
+import { PartsService } from '../service/parts.service';
+import { Observable } from 'rxjs';
 
 @Controller('parts')
-export class PartsController {}
+export class PartsController {
+
+    constructor(private partService: PartsService){}
+
+    @Post('createPart')
+    createPart(@Body() part: Part, @Request() request):Observable<Part>{
+        const user= request.user;
+        return this.partService.createPart(part, user);
+    }
+
+    @Put(':id')
+    updatePart(@Body() part: Part, @Param('id') id:number):Observable<Part>{
+        return this.partService.updatePart(Number(id),part);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id:number):Observable<Part>{
+        return this.partService.findOne(Number(id)); 
+    }
+
+    @Get()
+    findAll():Observable<Part[]>{
+        return this.partService.findAll();
+    }
+
+    @Delete(':id')
+    deletePart(@Param('id') id:number):Observable<Part>{
+        return this.partService.deletePart(Number(id));
+    }
+
+    @Get('user/:user')
+    findAllPartsByUser(@Param('user') user:number):Observable<Part[]>{
+        return this.partService.findAllPartsByUser(user);
+    }
+}
--- src/parts/service/parts.service.ts
+++ src/parts/service/parts.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { Part } from '../model/part.interface';
+import { User } from 'src/users/model/user.interface';
+import { PartEntity } from '../model/part.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Observable, from, map, switchMap } from 'rxjs';
 
 @Injectable()
-export class PartsService {}
+export class PartsService {
+    
+    constructor(
+        @InjectRepository(PartEntity) private readonly partRepository:Repository<PartEntity>
+    ){}
+
+    createPart(part: Part, user: User) {
+        part.user = user
+        return from(this.partRepository.save(part));
+    }
+
+    updatePart(id: number, part: Part): Observable<PartEntity> {
+        console.log('first update')
+        return from(this.partRepository.update(id, part)).pipe(
+            switchMap(()=>this.findOne(id))
+            )
+        ;
+    }
+
+    findOne(id: number): Observable<Part> {
+        console.log('first find')
+        return from(this.partRepository.findOneBy({id}));
+    }
+
+    findAll(): Observable<Part[]> {
+     return from(this.partRepository.find());   
+    }
+
+    deletePart(id: number): Observable<any> {
+       return from(this.partRepository.delete({id}));
+    }
+
+    findAllPartsByUser(user: number): Observable<Part[]> {
+        return from(this.partRepository.find({
+            where:{
+                user: {id: user}
+            },
+            relations:['user']
+        })).pipe(
+            map((part:PartEntity[]) =>part
+        ))
+    }
+    
+    
+  
+
+}
